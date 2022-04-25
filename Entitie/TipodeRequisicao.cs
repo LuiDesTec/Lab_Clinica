d81d25358b6f48a7701ab2883ae36a6d21c30da1
Luiz Carlos <luidestec@outlook.com>
2022-04-25 19:04:05 -0300

    Classe TipodeRequisicao

---
+++ Entitie/TipodeRequisicao.cs
@@ -0,0 +1,16 @@
+﻿using Entitie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Clinic.Entitie
+{
+     class TipodeRequisicao
+    {
+        public string Sus { get; set; }
+        public Convenio Convenio { get; set; }
+        public string Particular { get; set; }
+    }
+}
